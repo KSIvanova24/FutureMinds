939b5b821596477b16cd74bdcd4e693aae9754ea
SDTanev24 <SDTanev24@codingburgas.bg>
2026-03-11 21:20:36 +0200

    Setup raylib

---
+++ FutureMinds/ConsoleApplication1/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include "raylib.h"
+#include "raymath.h"
+#include "raylib.h"
+#include <string>
+#include <vector>
+#include <set>
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
